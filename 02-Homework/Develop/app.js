fdb5d33b20eb154995c8fbe1d9f477318d7a6a5d
Cate Dunn <60521577+CateDunn@users.noreply.github.com>
2020-04-26 13:09:50 -0400

    adding inquirer prompts

--- 02-Homework/Develop/app.js
+++ 02-Homework/Develop/app.js
@@ -1,28 +1,119 @@
+const Employee = require('./lib/Employee')
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const util = require('util');
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const writeFileAsync = util.promisify(fs.writeFile)
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
+class Main {
+    constructor() {
+        this.teamArray = [];
+    }
 
+    async team() {
+        let teamHTMLString = '';
+        for (const teamMember of this.teamArray) {
+            teamHTMLString += teamMember.team()
+        }
+        const result = Main.templateStart + teamHTMLString + Main.templateEnd
 
+        await writeFileAsync(path.resolve(__dirname, '..', 'output', 'generated.html'), result);
+    }
+    
+    async run() {
+        const { teamSize } = await inquirer.prompt([{
+            type: 'input',
+            name: 'teamSize',
+            message: 'Please input your team size',
+            default: 2,
+        }]);
 
+        for (let i = 0; i < teamSize; i++) {
+            console.log("======================")
+            const response = await inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'name',
+                    message: 'Enter team member name',
+                },
+                {
+                    type: 'input',
+                    name: 'id',
+                    message: 'Enter team member id',
+                },
+                {
+                    type: 'input',
+                    name: 'email',
+                    message: 'Enter team member email',
+                },
+                {
+                    type: 'list',
+                    name: 'role',
+                    message: "Choose team member's role",
+                    choices: [
+                        'Engineer',
+                        'Intern',
+                        'Manager',
+                    ]
+                },
+                {
+                        type: 'input',
+                        name: 'github',
+                        message: "Enter engineer's Github user name",
+                        when: ({ role })=> role === "Engineer"
+                },
+                {
+                    type: 'input',
+                    name: 'school',
+                    message: "Enter intern's school",
+                    when: ({ role })=> role === "Intern"
+                },
+                {
+                    type: 'input',
+                    name: 'officeNumber',
+                    message: "Enter manager's office number",
+                    when: ({ role })=> role === "Manager"
+                },
+            ]);
+            
+            const {
+                name,
+                id,
+                email,
+                role,
+                github,
+                school,
+                officeNumber,
+            } = response;
 
+            if (role == 'Engineer') {
+                this.teamArray.push(new Engineer(name, id, email, github))
+            }
+            if (role == 'Manager') {
+                this.teamArray.push(new Manager(name, id, email, officeNumber))
+            }
+            if (role == 'Intern') {
+                this.teamArray.push(new Intern(name, id, email, school))
+            }
+        }
+        await this.team();
+    }
+}
 
-
-
-
-
+const program = new Main();
+program.run();
 
 
 // After the user has input all employees desired, call the `render` function (required
